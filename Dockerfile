136eb6c5bfde42a31d21fc6b6e753793ad9f4aa4
jasonayre <jasonayre@gmail.com>
2022-01-13 17:06:25 -0700

    begin portainer service restarter

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,15 @@
 FROM ubuntu:latest
-MAINTAINER docker@ekito.fr
+MAINTAINER jayre@reactiondata.com
 
 # Add crontab file in the cron directory
-ADD crontab /etc/cron.d/hello-cron
+RUN mkdir -p /home/bin
+COPY update_portainer_service.sh /home/bin/update_portainer_service.sh
+RUN chmod 0644 /home/bin/update_portainer_service.sh
+ADD run_cron /etc/cron.d/run_cron
+
 
 # Give execution rights on the cron job
-RUN chmod 0644 /etc/cron.d/hello-cron
+RUN chmod 0644 /etc/cron.d/run_cron
 
 # Create the log file to be able to run tail
 RUN touch /var/log/cron.log
@@ -13,7 +17,6 @@ RUN touch /var/log/cron.log
 #Install Cron
 RUN apt-get update
 RUN apt-get -y install cron
-
 
 # Run the command on container startup
 CMD cron && tail -f /var/log/cron.log
---
+++ update_portainer_service.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]
+  then
+    echo "you need curl an jq installed"
+    echo "USERNAME PASSWORD HOST SERVICE_NAME IMAGE_NAME"
+    exit 0
+fi
+
+USERNAME=$PORTAINER_USERNAME
+PASSWORD=$PORTAINER_PASSWORD
+HOST=$PORTAINER_HOST
+SERVICE_NAME=$PORTAINER_SERVICE_NAME
+IMAGE_NAME=$PORTAINER_IMAGE_NAME
+
+LOGIN_TOKEN=$(curl -k -s -H "Content-Type: application/json" -d "{\"username\":\"$USERNAME\",\"password\":\"$PASSWORD\"}" -X POST $HOST/api/auth | jq -r .jwt)
+ENDPOINT_ID=$(curl -k -s -H "Authorization: Bearer $LOGIN_TOKEN" $HOST/api/endpoints | jq ."[].Id")
+SERVICE=$(curl -k -s -H "Authorization: Bearer $LOGIN_TOKEN" $HOST/api/endpoints/${ENDPOINT_ID}/docker/services | jq -c ".[] | select( .Spec.Name==(\"$SERVICE_NAME\"))")
+
+ID=$(echo $SERVICE | jq  -r .ID)
+SPEC=$(echo $SERVICE | jq .Spec)
+VERSION=$(echo $SERVICE | jq .Version.Index)
+FORCE_UPDATE=$(echo $SPEC | jq  ".TaskTemplate.ForceUpdate" | bc)
+let "FORCE_UPDATE=${FORCE_UPDATE}+1"
+UPDATE=$(echo $SPEC | jq ".TaskTemplate.ContainerSpec.Image |= \"$IMAGE_NAME\" " | jq ".TaskTemplate.ForceUpdate = ${FORCE_UPDATE} ")
+
+curl -k -H "Content-Type: application/json" \
+-H "Authorization: Bearer $LOGIN_TOKEN" -X POST -d "${UPDATE}" \
+"$HOST/api/endpoints/${ENDPOINT_ID}/docker/services/$ID/update?version=$VERSION"

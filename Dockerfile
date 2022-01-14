a6f0b458e784eea96bfb68bbb750612f464c5635
jasonayre <jasonayre@gmail.com>
2022-01-14 16:34:28 -0700

    allow args to be passed into update script, cron working now

--- Dockerfile
+++ Dockerfile
@@ -3,11 +3,29 @@ MAINTAINER jayre@reactiondata.com
 
 # Add crontab file in the cron directory
 RUN mkdir -p /home/bin
+ARG PORTAINER_HOST
+ARG PORTAINER_PASSWORD
+ARG PORTAINER_USERNAME
+ARG PORTAINER_IMAGE_NAME
+ARG PORTAINER_SERVICE_NAME
+#ENV PORTAINER_HOST=$PORTAINER_HOST
+RUN touch /etc/profile
+RUN echo "export PORTAINER_HOST='${PORTAINER_HOST}'\n" >> /etc/profile
+RUN echo "export PORTAINER_PASSWORD='${PORTAINER_PASSWORD}'\n" >> /etc/profile
+RUN echo "export PORTAINER_USERNAME='${PORTAINER_USERNAME}'\n" >> /etc/profile
+RUN echo "export PORTAINER_IMAGE_NAME='${PORTAINER_IMAGE_NAME}'\n" >> /etc/profile
+RUN echo "export PORTAINER_SERVICE_NAME='${PORTAINER_SERVICE_NAME}'\n" >> /etc/profile
+ENV SOMETHING=anything
+RUN echo "${SOMETHING}" >> /root/env
+RUN echo $SOMETHING >> /root/env
 COPY update_portainer_service.sh /home/bin/update_portainer_service.sh
-RUN chmod 0644 /home/bin/update_portainer_service.sh
+
+RUN chmod u+x /home/bin/update_portainer_service.sh
 ADD run_cron /etc/cron.d/run_cron
 
-
+#RUN echo "export PORTAINER_HOST=${PORTAINER_HOST}" > /etc/profile
+#RUN echo $PORTAINER_HOST > /etc/profile
+#RUN echo "${PORTAINER_HOST}" > /etc/profile
 # Give execution rights on the cron job
 RUN chmod 0644 /etc/cron.d/run_cron
 
@@ -17,6 +35,9 @@ RUN touch /var/log/cron.log
 #Install Cron
 RUN apt-get update
 RUN apt-get -y install cron
-
+RUN apt-get -y install jq
+RUN apt-get -y install curl
+RUN apt-get -y install bc
 # Run the command on container startup
+
 CMD cron && tail -f /var/log/cron.log
--- update_portainer_service.sh
+++ update_portainer_service.sh
@@ -1,27 +1,40 @@
 #!/bin/bash
 
-if [ $# -eq 0 ]
-  then
-    echo "you need curl an jq installed"
-    echo "USERNAME PASSWORD HOST SERVICE_NAME IMAGE_NAME"
-    exit 0
-fi
+source /etc/profile
 
 USERNAME=$PORTAINER_USERNAME
 PASSWORD=$PORTAINER_PASSWORD
 HOST=$PORTAINER_HOST
 SERVICE_NAME=$PORTAINER_SERVICE_NAME
 IMAGE_NAME=$PORTAINER_IMAGE_NAME
+echo $HOST
 
 LOGIN_TOKEN=$(curl -k -s -H "Content-Type: application/json" -d "{\"username\":\"$USERNAME\",\"password\":\"$PASSWORD\"}" -X POST $HOST/api/auth | jq -r .jwt)
+echo $LOGIN_TOKEN
+echo $HOST
 ENDPOINT_ID=$(curl -k -s -H "Authorization: Bearer $LOGIN_TOKEN" $HOST/api/endpoints | jq ."[].Id")
+echo $ENDPOINT_ID
+echo $SERVICE_NAME
+curl -k -s -H "Authorization: Bearer $LOGIN_TOKEN" "$HOST/api/endpoints/${ENDPOINT_ID}/docker/services"
 SERVICE=$(curl -k -s -H "Authorization: Bearer $LOGIN_TOKEN" $HOST/api/endpoints/${ENDPOINT_ID}/docker/services | jq -c ".[] | select( .Spec.Name==(\"$SERVICE_NAME\"))")
 
+echo "SERVICE IS"
+echo $SERVICE
+
 ID=$(echo $SERVICE | jq  -r .ID)
+echo "ID IS"
+echo $ID
 SPEC=$(echo $SERVICE | jq .Spec)
+echo "SPEC IS"
+echo $SPEC
 VERSION=$(echo $SERVICE | jq .Version.Index)
+echo $VERSION
 FORCE_UPDATE=$(echo $SPEC | jq  ".TaskTemplate.ForceUpdate" | bc)
+echo "FORCE UPDATE WAS"
+echo $FORCE_UPDATE
 let "FORCE_UPDATE=${FORCE_UPDATE}+1"
+echo "FORCE UPDATE IS"
+echo $FORCE_UPDATE
 UPDATE=$(echo $SPEC | jq ".TaskTemplate.ContainerSpec.Image |= \"$IMAGE_NAME\" " | jq ".TaskTemplate.ForceUpdate = ${FORCE_UPDATE} ")
 
 curl -k -H "Content-Type: application/json" \
